a99ce72335d65fb49f417fa56dc304bd1f8090f0
Alexandr1818 <samsungi777s@gmail.com>
2018-03-13 19:13:16 +0200

    hw8btsp

---
+++ assets/js/common.js
@@ -0,0 +1,15 @@
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  // type: string,
+  default:'slick-dots',
+  prevArrow:'<button type="button" class="slick-prev">Previous</button>',
+  nextArrow:'<button type="button" class="slick-next">Next</button>'
+  // dotsClass:
+});
+
+  $('.slider1').slick({
+    dots: true,
+    infinite: true
+
+});
